839bd25fe7af756a7521e00ce0504323edbcd602
RimidalU <stankevichuuladimiru@gmail.com>
2024-04-17 18:51:38 +0300

    feat: implement pagination logic

--- web/src/components/pagination/pagination.tsx
+++ web/src/components/pagination/pagination.tsx
@@ -8,11 +8,25 @@ export interface IPaginationComponent {
 }
 
 export const PaginationComponent = (props: IPaginationComponent) => {
+  const DISPLAYED_PAGES_COUNT = 10;
+  const halfPagination = DISPLAYED_PAGES_COUNT / 2;
+
   const { currentPage, setCurrentPage, totalPages, pageLimit } = props;
 
   let pageItems = [];
+  let startPage = currentPage - halfPagination;
+  let endPage = currentPage + halfPagination - 1;
 
-  for (let page = 1; page <= totalPages; page++) {
+  if (currentPage <= halfPagination) {
+    startPage = 1;
+    endPage = DISPLAYED_PAGES_COUNT;
+  }
+  if (currentPage >= totalPages - halfPagination + 1) {
+    startPage = totalPages - DISPLAYED_PAGES_COUNT + 1;
+    endPage = totalPages;
+  }
+
+  for (let page = startPage; page <= endPage; page++) {
     if (currentPage !== page) {
       pageItems.push(
         <Pagination.Item key={page} onClick={() => setCurrentPage(page)}>
@@ -34,7 +48,7 @@ export const PaginationComponent = (props: IPaginationComponent) => {
       <Pagination.Prev disabled={currentPage === 1} onClick={() => setCurrentPage(currentPage - 1)} />
       {pageItems}
       <Pagination.Next disabled={currentPage === totalPages} onClick={() => setCurrentPage(currentPage + 1)} />
-      <Pagination.Next disabled={currentPage === totalPages} onClick={() => setCurrentPage(totalPages)} />
+      <Pagination.Last disabled={currentPage === totalPages} onClick={() => setCurrentPage(totalPages)} />
     </Pagination>
   );
 };
